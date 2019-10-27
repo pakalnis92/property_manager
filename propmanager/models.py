7e9a2d64b7cafe61a5318d6ff5cccd8b3ba70c24
Karolis Pakalnis <karolpakaln@gmail.com>
2019-10-27 15:10:51 +0000

    property manager task

--- propmanager/models.py
+++ propmanager/models.py
@@ -6,8 +6,11 @@ from django.db import models
 # Create your models here.
 
 class Owner(models.Model):
-    first_name = models.CharField(max_length=100, default="N/A")
-    last_name = models.CharField(max_length=100, default="N/A")
+    first_name = models.CharField(max_length=50, default="N/A")
+    last_name = models.CharField(max_length=50, default="N/A")
+    email = models.CharField(max_length=20, default="N/A")
+    telephone = models.CharField(max_length=20, default="N/A")
+
 
     def __str__(self):
         return 'Owner full name: %s %s' % (self.first_name, self.last_name)
@@ -24,16 +27,6 @@ class Property(models.Model):
     """
     Model to represent the property.
     """
-
-    PROPERTY_TYPE_COMMERCIAL = "commercial"
-    PROPERTY_TYPE_SEMI = "semi_commercial"
-    PROPERTY_TYPE_RESIDENTIAL = "residential"
-
-    PROPERTY_TYPE_LIST = (
-        (PROPERTY_TYPE_COMMERCIAL, "Commercial"),
-        (PROPERTY_TYPE_SEMI, "Semi-commercial"),
-        (PROPERTY_TYPE_RESIDENTIAL, "Residential")
-    )
     address_line_1 = models.CharField(max_length=100, default="N/A")
     address_line_2 = models.CharField(max_length=100, default="N/A")
     city_town = models.CharField(max_length=100, default="N/A")
